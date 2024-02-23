968d1670acde4f4bd524d4f3350add327b60391e
antirez <antirez@gmail.com>
2024-02-23 19:40:37 +0100

    Proper init with features configuration loading.

--- bma423.py
+++ bma423.py
@@ -16,12 +16,24 @@
 from machine import Pin
 
 # Registers
-REG_CMD = const(0x7e)       # Write there to send commands
-REG_PWR_CTL = const(0x7d)   # Used to power-on the device parts
-REG_PWR_CONF = const(0x73)
+REG_CHIP_ID = const(0x00)       # Chip identification number.
+REG_INT_STATUS_0 = const(0x1C)  # Interrupt status for features detection.
+REG_INT_STATUS_1 = const(0x1D)  # Interrupt status for data ready.
+REG_STEP_COUNTER_1 = const(0x1F) # For bytes starting here. Number of steps.
+REG_TEMPERATURE = const(0x22) # Temperature sensor reading: kelvin units.
+REG_INTERNAL_STATUS = const(0x2A) # Error / status bits.
 REG_ACC_CONF = const(0x40)  # Out data rate, bandwidth, read mode.
 REG_ACC_RANGE = const(0x41) # Acceleration range selection.
-REG_TEMPERATURE = const(0x22) # Temperature sensor reading: kelvin units.
+REG_PWR_CONF = const(0x7c)  # Power mode configuration.
+REG_PWR_CTL = const(0x7d)   # Used to power-on the device parts
+REG_CMD = const(0x7e)       # Write there to send commands
+REG_INT1_IO_CTRL = const(0x53) # Electrical config of interrupt 1 pin.
+REG_INT2_IO_CTRL = const(0x54) # Electrical config of interrupt 2 pin.
+REG_INT_LATCH = const(0x55) # Interrupt latch mode.
+REG_INT1_MAP = const(0x56)  # Interrput map for detected features and pin1.
+REG_INT2_MAP = const(0x57)  # Interrput map for detected features and pin2.
+REG_INT_MAP_DATA = const(0x58) # Interrupt map for pin1/2 data events.
+REG_INIT_CTRL = const(0x59) # Initialization register.
 
 # Commands for the REG_CMD register
 REG_CMD_SOFTRESET = const(0xB6)
@@ -31,14 +43,11 @@ class BMA423:
     # 2G, 4G, 8G and 16G. If we want to be able to measure higher
     # max accelerations, the relative precision decreases as we have
     # a fixed 12 bit reading.
-    def __init__(self,i2c,*,acc_range=4,interrupt_pin=None,callback=None):
+    def __init__(self,i2c,*,acc_range=4):
         default_addr = [0x18,0x19] # Changes depending on SDO pin
                                    # pulled to ground or V+
         self.i2c = i2c
         self.myaddr = None
-        self.callback = callback
-        self.interrupt_pin = interrupt_pin
-        self.interrupt_pin.irq(handler=self.irq, trigger=Pin.IRQ_FALLING)
 
         found_devices = i2c.scan()
         print("BMA423: scan i2c bus:", [hex(x) for x in found_devices])
@@ -48,9 +57,38 @@ class BMA423:
                 break
         if self.myaddr == None:
             raise Exception("BMA423 not found at i2c bus")
-        print("BMA423: device found at ",hex(self.myaddr))
+        print("BMA423: device with matching address found at",hex(self.myaddr))
 
-        # Initialization.
+        # Device initialization.
+        self.set_reg(REG_CMD,REG_CMD_SOFTRESET) # Reset the chip.
+        time.sleep(1)
+
+        chip_id = self.get_reg(REG_CHIP_ID)
+        if chip_id != 0x13:
+            raise Exception("BMA423 chip ID is not 0x13 as expected. Different sensor connected?")
+        print("BMA423: chip correctly identified.")
+
+        self.set_reg(REG_PWR_CONF,0x00)  # Disable adv_power_save.
+        time.sleep_us(500)               # Wait time synchronization.
+        self.set_reg(REG_INIT_CTRL,0x00) # Prepare for loading configuration.
+        self.transfer_config()           # Load binary features config.
+        self.set_reg(REG_INIT_CTRL,0x01) # Enable features.
+        time.sleep_ms(140)               # Wait ASIC initialization.
+
+        # The chip is ready for further configuration when the
+        # status "message" turns 1.
+        wait_epoch = 0
+        while True:
+            status = self.get_reg(REG_INTERNAL_STATUS) & 0b11111
+            if status == 1: break # Initialization successful
+            time.sleep_ms(50)
+            wait_epoch += 1
+            if wait_epoch == 20:
+                raise Exception("Timeout during init, internal_status: ",
+                    status)
+        print("BMA423: device initialized successfully. Configuring...")
+
+        # Configure device.
         self.set_reg(REG_PWR_CTL,0x04) # acquisition enabled, aux disabled.
 
         # Enable performance mode: in this mode, the data sampling happens
@@ -65,6 +103,98 @@ class BMA423:
     
         # Enable 4G range.
         self.set_range(acc_range)
+
+    # The BMA423 features detection requires that we transfer a binary
+    # blob via the features configuration register (and other two undocumented
+    # registers that set the internal target address at which the register
+    # points). If this are the weights of a small neural network, or just
+    # parameters, I'm not sure. More info (LOL, not really) here:
+    #
+    # https://github.com/boschsensortec/BMA423_SensorDriver
+    def transfer_config(self):
+        print("Uploading features configuration...")
+        f = open("bma423conf.bin","rb")
+        buf = bytearray(8) # Binary config is multiple of 8 in len.
+        idx = 0
+        while f.readinto(buf,8) == 8:
+            # The index of the half-word (so index/2) must
+            # be placed into this two undocumented registers
+            # 0x5B and 0x5C. Data goes in 0xE.
+            # Thanks for the mess, BOSH!
+            self.set_reg(0x5b,(idx//2)&0xf) # Set LSB (bits 3:0)
+            self.set_reg(0x5c,(idx//2)>>4)  # Set MSB (bits 11:5)
+            self.set_reg(0x5e,buf)
+            idx += 8
+        print("Done: total transfer: ", idx)
+
+        # Verify the content.
+        print("BMA423: Verifying stored configuration...")
+        idx = 0
+        f.seek(0)
+        while f.readinto(buf,8) == 8:
+            self.set_reg(0x5b,(idx//2)&0xf) # Set LSB (bits 3:0)
+            self.set_reg(0x5c,(idx//2)>>4)  # Set MSB (bits 11:5)
+            content = self.get_reg(0x5e,8)
+            idx += 8
+            if content != buf:
+                raise Exception("Feature config data mismatch at",idx)
+        f.close()
+
+    # Enable interrupt for the specified list of events.
+    #
+    # 'chip_pin' is 1 or 2 (the chip has two interrupt pins), you should
+    # select the one you want to use or the one you have an actual
+    # connection to with your host.
+    # 'pin' is your machine.Pin instance in your host.
+    # 'callback' is the function to call when the specified events will fire.
+    # 'events' is a list of strings specifying what events you want to
+    # listen for. Valid events are:
+    #    "data": new acceleration reading available.
+    #    "fifo-wm: fifo watermark reached.
+    #    "fifo-full": fifo is full.
+    #    "step": step feature.
+    #    "activity": detect walking, running, ...
+    #    "tilt": tilt on wrist.
+    #    "double-tap": double tap.
+    #    "single-tap": single tap.
+    #    "any-none": any motion / no motion detected.
+    # Note: you can't subscribe to both double and single tap.
+    def enable_interrupt(self,chip_pin,pin,callback,events):
+        self.callback = callback
+
+        # Features detection only work in latch mode.
+        self.set_reg(REG_INT_LATCH,0x01)
+        # feature name -> bit to set in INT1/2_MAP.
+        feature_bits = {"any-none":6,"tilt":3,"activity":2,"step":1}
+        # data source name -> bit to set for [pin1,pin2] in
+        # INT_MAP_DATA.
+        data_bits = {"data":[2,6],"fifo-wm":[1,5],"fifo-full":[0,4]}
+
+        # Set features/data interrupt maps register values.
+        feature_map,data_map = 0,0
+        for e in events:
+            if e in feature_bits:
+                feature_map |= (1 << feature_bits[e])
+            elif e in data_bits:
+                data_map |= data_bits[e][pin-1]
+            else:
+                raise Exception(f"Unknown event {e} when enabling interrupt.")
+        if feature_map != 0:
+            map_int_reg = REG_INT1_MAP if chip_pin == 1 else REG_INT2_MAP
+            self.set_reg(map_int_reg,feature_map)
+        if data_map != 0: self.set_reg(REG_INT_MAP_DATA,data_map)
+
+        # XXX: set config registers according to single/double tap.
+        # XXX: set FEATURES_IN registers.
+
+        # Configure the electrical interrput pin behavior.
+        ctrl_reg = REG_INT1_IO_CTRL if chip_pin == 1 else REG_INT2_IO_CTRL
+        # Output enabled 0x8, active high 0x2, all other bits zero, that
+        # is: input_enabled=no, edge_ctrl=level-trigger, od=push-pull.
+        self.set_reg(ctrl_reg, 0x8 | 0x2)
+
+        # Finally enable the interrupt in the host pin.
+        pin.irq(handler=self.irq, trigger=Pin.IRQ_RISING)
 
     # Set range of 2, 4 or 8 or 16g
     def set_range(self,acc_range):
@@ -89,7 +219,7 @@ class BMA423:
         return self.range / 2047 * reading
 
     # Return x,y,z acceleration.
-    def getxyz(self):
+    def get_xyz(self):
         rawdata = self.get_reg(0x12,6)
         acc_x = (rawdata[0] >> 4) | (rawdata[1] << 4)
         acc_y = (rawdata[2] >> 4) | (rawdata[3] << 4)
@@ -102,12 +232,24 @@ class BMA423:
         acc_z = self.normalize_reading(acc_z)
         return (acc_x,acc_y,acc_z)
 
+    # Return the chip tempereature in celsius.
+    # If the temperature is invalid, None is returned.
+    def get_temperature(self):
+        raw = self.get_reg(REG_TEMPERATURE)
+        if raw == 0x80: return None
+        if raw & 0x80:
+            raw = -((~raw)+1) # Conver 2 complement to signed integer.
+        return 23+raw
+
     def irq(self,pin):
         if self.callback == None:
-            printf("BMA423: not handled IRQ. Pass 'callback' during initialization")
+            printf("BMA423: not handled IRQ. Please, set a callback.")
             return
-        data = self.get_touch_coords()
-        if data == None: return
+        data = {}
+
+        print("IRQ CALLED")
+
+        if len(data) == None: return
         self.callback(data)
 
     # Return the single byte at the specified register
@@ -119,15 +261,23 @@ class BMA423:
 
     # Write 'value' to the specified register
     def set_reg(self, register, value):
-        self.i2c.writeto_mem(self.myaddr,register,bytes([value]))
+        if isinstance(value,bytearray) or isinstance(value,bytes):
+            self.i2c.writeto_mem(self.myaddr,register,value)
+        else:
+            self.i2c.writeto_mem(self.myaddr,register,bytes([value]))
 
 # Example usage and quick test to see if your device is working.
 if  __name__ == "__main__":
     from machine import SoftI2C, Pin
     import time
 
+    # Called when a feature/data interrupt triggers.
+    def mycallback(data):
+        print(data)
+
     i2c = SoftI2C(scl=11,sda=10)
-    sensor = BMA423(i2c,interrupt_pin=Pin(14,Pin.IN),callback=None)
+    sensor = BMA423(i2c)
+    sensor.enable_interrupt(1,Pin(14,Pin.IN),mycallback,["step"])
     while True:
-        print(sensor.getxyz())
+        print("(x,y,z),temp",sensor.get_xyz(),sensor.get_temperature())
         time.sleep(.1)
